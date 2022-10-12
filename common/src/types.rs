42cd7a85f9d4475dc9243f97164604f5ef07ef58
marioiordanov <mio@shardlabs.io>
2022-10-12 14:10:29 +0300

    common prj

---
+++ common/src/types.rs
@@ -0,0 +1,14 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+use serde::{Serialize, Deserialize};
+use uint::construct_uint;
+
+pub type DrawId = u128;
+pub type NumPicks = u128;
+
+construct_uint!{
+    /// 256-bit unsigned integer
+    #[derive(Serialize, Deserialize, BorshDeserialize, BorshSerialize)]
+    pub struct U256(4);
+}
+
+pub type WinningNumber = U256;
